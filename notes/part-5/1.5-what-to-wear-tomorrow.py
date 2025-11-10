fe90b4b9268ab331d44e1d81e55a05a40cb520b7
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2025-11-11 01:59:49 +0800

    Create 1.5-what-to-wear-tomorrow.py

---
+++ notes/part-5/1.5-what-to-wear-tomorrow.py
@@ -0,0 +1,55 @@
+## Please write a program which asks for tomorrow's weather forecast and then suggests weather-appropriate clothing.
+
+# The suggestion should change if the temperature (measured in degrees Celsius) is over 20, 10 or 5 degrees,
+# and also if there is rain on the radar.
+
+# Some examples of expected behaviour:
+
+## Sample output
+# What is the weather forecast for tomorrow?
+# Temperature: -> input: '21'
+# Will it rain (yes/no): -> input: 'no'
+# Wear jeans and a T-shirt
+
+## Sample output
+# What is the weather forecast for tomorrow?
+# Temperature: -> input: '11'
+# Will it rain (yes/no): -> input: 'no'
+# Wear jeans and a T-shirt
+# I recommend a jumper as well
+
+## Sample output
+# What is the weather forecast for tomorrow?
+# Temperature: -> input: '7'
+# Will it rain (yes/no): -> input: 'no'
+# Wear jeans and a T-shirt
+# I recommend a jumper as well
+# Take a jacket with you
+
+## Sample output
+# What is the weather forecast for tomorrow?
+# Temperature: -> input: '3'
+# Will it rain (yes/no): -> input: 'yes'
+# Wear jeans and a T-shirt
+# I recommend a jumper as well
+# Take a jacket with you
+# Make it a warm coat, actually
+# I think gloves are in order
+# Don't forget your umbrella!
+
+
+## Solution:
+print("What is the weather forecast for tomorrow?")
+temperature = int(input("Temperature:"))
+rain = input("Will it rain (yes/no):")
+
+print("Wear jeans and a T-shirt")
+if temperature <=20:
+    print("I recommend a jumper as well")
+if temperature <=10:
+    print("Take a jacket with you")
+if temperature <=5:
+    print("Make it a warm coat, actually\nI think gloves are in order")
+
+if rain == "yes":
+    print("Don't forget your umbrella!")
