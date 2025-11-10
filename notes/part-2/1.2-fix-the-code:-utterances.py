2178053df765ca4d994cc0d6271e946e3fc91568
wuthree00 <22486996+wuthree00@users.noreply.github.com>
2025-11-11 01:24:11 +0800

    Create 1.2-fix-the-code:-utterances.py

---
+++ notes/part-2/1.2-fix-the-code:-utterances.py
@@ -0,0 +1,15 @@
+## Here is a program which should ask for three utterances and print them out, like so:
+
+## Sample output
+# The 1st part: hickory
+# The 2nd part: dickory
+# The 3rd part: dock
+# hickory-dickory-dock!
+
+# However, there is something wrong with the code below. Please fix it.
+
+## Solution:
+part1 = input("The 1st part: ")
+part2 = input("The 2nd part: ")
+part3 = input("The 3rd part: ")
+print(part1+"-"+part2 + "-"+part3+"!")
